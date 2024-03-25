1d91e0eb14ea43045994cdfa2b8da52ef057ae29
vineaugustus <vineonis@gmail.com>
2024-03-25 16:44:26 -0300

    projeto soma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SomaComponent } from './pages/soma';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterOutlet, SomaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
